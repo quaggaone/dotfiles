4985755b2c630e9ee634cb0f67278d157c281828
Felix Dimmerling <ahoi@felixdim.com>
2025-06-19 15:34:41 +0200

    ADD UI navigation using the keyboard

--- defaults/system/keyboard.sh
+++ defaults/system/keyboard.sh
@@ -5,4 +5,5 @@ source "$(dirname "$0")/../utils.sh"
 
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
 
