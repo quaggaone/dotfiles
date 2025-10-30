4626b90591ee63e7ee7fdb1668efa5a5ead392f6
Felix Dimmerling <ahoi@felixdim.com>
2025-10-30 22:00:38 +0100

    ADJUST battery monitoring settings

--- .config/sketchybar/items/battery.sh
+++ .config/sketchybar/items/battery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sketchybar --add alias "Control Center,Battery" right \
-    --set "Control Center,Battery" update_freq=120 script="$PLUGIN_DIR/battery.sh" \
+    --set "Control Center,Battery" update_freq=60 script="$PLUGIN_DIR/battery.sh" \
     --subscribe "Control Center,Battery" system_woke power_source_change
