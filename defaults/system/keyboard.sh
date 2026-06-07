7e616d0d8c86b66613972398c21da3abc785d6dc
Felix Dimmerling <ahoi@felixdim.com>
2026-06-07 17:57:11 +0200

    disable keyboard auto replacement

--- defaults/system/keyboard.sh
+++ defaults/system/keyboard.sh
@@ -6,4 +6,5 @@ source "$(dirname "$0")/../utils.sh"
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
+defaults write NSGlobalDomain NSAutomaticInlinePredictionEnabled -bool false
 
