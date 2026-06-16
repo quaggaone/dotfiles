afd8c0aba0c7b8d83ac51f838e92b9f9944f014c
Felix Dimmerling <ahoi@felixdim.com>
2026-06-16 15:15:12 +0200

    ADD markedit autocorrect setting

--- defaults/markedit.sh
+++ defaults/markedit.sh
@@ -3,3 +3,4 @@
 # symlink settings file
 ln -siF ~/dotfiles/.config/markedit/settings.json $HOME/Library/Containers/app.cyan.markedit/Data/Documents/settings.json
 
+defaults write app.cyan.markedit WebAutomaticSpellingCorrectionEnabled -bool false
