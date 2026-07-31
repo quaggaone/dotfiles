6c4d2afa80d8a2ab626c728ca825738cde39e9c9
Felix Dimmerling <ahoi@felixdim.com>
2026-07-31 11:50:41 +0200

    markedit customizations

--- defaults/markedit.sh
+++ defaults/markedit.sh
@@ -2,5 +2,6 @@
 
 # symlink settings file
 ln -siF ~/dotfiles/.config/markedit/settings.json $HOME/Library/Containers/app.cyan.markedit/Data/Documents/settings.json
+ln -siF ~/dotfiles/.config/markedit/editor.css $HOME/Library/Containers/app.cyan.markedit/Data/Documents/editor.css
 
 defaults write app.cyan.markedit WebAutomaticSpellingCorrectionEnabled -bool false
