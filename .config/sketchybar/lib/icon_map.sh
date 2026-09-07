bfe6d801016a730955e2d8dd37b9fe2a11fb2152
Felix Dimmerling <ahoi@felixdim.com>
2026-09-07 10:20:59 +0200

    update sketchybar-app-font icon map

--- .config/sketchybar/lib/icon_map.sh
+++ .config/sketchybar/lib/icon_map.sh
@@ -6,6 +6,9 @@ function __icon_map() {
    "Live")
         icon_result=":ableton:"
         ;;
+   "Accessibility Reader")
+        icon_result=":accessibility_reader:"
+        ;;
    "Acrobat")
         icon_result=":acrobat:"
         ;;
@@ -128,6 +131,9 @@ function __icon_map() {
         ;;
    "Audio MIDI Setup")
         icon_result=":audio_midi_setup:"
+        ;;
+   "AusweisApp")
+        icon_result=":ausweisapp:"
         ;;
    "Automator")
         icon_result=":automator:"
@@ -288,7 +294,7 @@ function __icon_map() {
    "Clock")
         icon_result=":clock:"
         ;;
-   "cmux")
+   "cmux" | "cmux NIGHTLY")
         icon_result=":cmux:"
         ;;
    "coconutBattery")
@@ -377,6 +383,9 @@ function __icon_map() {
         ;;
    "Default Folder X")
         icon_result=":default_folder_x:"
+        ;;
+   "Delta")
+        icon_result=":delta:"
         ;;
    "Deluge")
         icon_result=":deluge:"
@@ -630,6 +639,9 @@ function __icon_map() {
    "Grayjay")
         icon_result=":grayjay:"
         ;;
+   "Grok Bot")
+        icon_result=":grok_bot:"
+        ;;
    "Hammerspoon")
         icon_result=":hammerspoon:"
         ;;
@@ -774,6 +786,9 @@ function __icon_map() {
    "KeePassXC")
         icon_result=":kee_pass_x_c:"
         ;;
+   "KensingtonWorks" | "KensingtonWorks2")
+        icon_result=":kensington_works:"
+        ;;
    "Keyboard Maestro")
         icon_result=":keyboard_maestro:"
         ;;
@@ -788,6 +803,9 @@ function __icon_map() {
         ;;
    "Kiro")
         icon_result=":kiro:"
+        ;;
+   "Kiro Crew")
+        icon_result=":kiro_crew:"
         ;;
    "kitty")
         icon_result=":kitty:"
@@ -891,6 +909,9 @@ function __icon_map() {
    "Marked 2")
         icon_result=":marked_2:"
         ;;
+   "MarkText" | "MarkText+")
+        icon_result=":marktext:"
+        ;;
    "Marta")
         icon_result=":marta:"
         ;;
@@ -993,11 +1014,17 @@ function __icon_map() {
    "Music" | "音乐" | "Musique" | "ミュージック" | "Musik" | "Chromatix" | "Музика" | "Музыка")
         icon_result=":music:"
         ;;
+   "Muxy")
+        icon_result=":muxy:"
+        ;;
    "Native Access")
         icon_result=":native_instruments:"
         ;;
    "Navicat Premium")
         icon_result=":navicat:"
+        ;;
+   "NeoHtop")
+        icon_result=":neohtop:"
         ;;
    "Neovide" | "neovide")
         icon_result=":neovide:"
@@ -1089,6 +1116,9 @@ function __icon_map() {
    "ONLYOFFICE")
         icon_result=":onlyoffice:"
         ;;
+   "OpenKnowledge")
+        icon_result=":open_knowledge:"
+        ;;
    "Open Video Downloader")
         icon_result=":open_video_downloader:"
         ;;
@@ -1128,6 +1158,9 @@ function __icon_map() {
    "Osaurus")
         icon_result=":osaurus:"
         ;;
+   "Otty")
+        icon_result=":otty:"
+        ;;
    "Overcast")
         icon_result=":overcast:"
         ;;
@@ -1148,6 +1181,9 @@ function __icon_map() {
         ;;
    "Parsec")
         icon_result=":parsec:"
+        ;;
+   "Paseo")
+        icon_result=":paseo:"
         ;;
    "Passepartout")
         icon_result=":passepartout:"
@@ -1265,6 +1301,9 @@ function __icon_map() {
         ;;
    "PS Remote Play")
         icon_result=":ps_remote_play:"
+        ;;
+   "PureMac")
+        icon_result=":puremac:"
         ;;
    "PyCharm")
         icon_result=":pycharm:"
@@ -1422,6 +1461,9 @@ function __icon_map() {
    "Signal")
         icon_result=":signal:"
         ;;
+   "Silveran Reader")
+        icon_result=":silveran_reader:"
+        ;;
    "sioyek")
         icon_result=":sioyek:"
         ;;
@@ -1451,6 +1493,9 @@ function __icon_map() {
         ;;
    "Sonarr")
         icon_result=":sonarr:"
+        ;;
+   "SourceGit")
+        icon_result=":sourcegit:"
         ;;
    "Sourcetree")
         icon_result=":sourcetree:"
@@ -1509,6 +1554,9 @@ function __icon_map() {
    "System Information" | "System Profiler")
         icon_result=":system_information:"
         ;;
+   "T3 Code" | "T3 Code (Alpha)")
+        icon_result=":t3_code:"
+        ;;
    "T3 Chat")
         icon_result=":t3chat:"
         ;;
@@ -1556,6 +1604,9 @@ function __icon_map() {
         ;;
    "Microsoft To Do" | "Things")
         icon_result=":things:"
+        ;;
+   "Threema"*)
+        icon_result=":threema:"
         ;;
    "Thunderbird" | "Thunderbird Daily")
         icon_result=":thunderbird:"
@@ -1613,6 +1664,9 @@ function __icon_map() {
         ;;
    "Trello")
         icon_result=":trello:"
+        ;;
+   "Tuna" | "Tuna (beta)")
+        icon_result=":tuna:"
         ;;
    "Tuple")
         icon_result=":tuple:"
@@ -1758,7 +1812,7 @@ function __icon_map() {
    "Z-Library")
         icon_result=":z_library:"
         ;;
-   "Zed")
+   "Zed" | "Zed Preview")
         icon_result=":zed:"
         ;;
    "Zen" | "Zen Browser" | "Twilight")
@@ -1767,7 +1821,7 @@ function __icon_map() {
    "Zeplin")
         icon_result=":zeplin:"
         ;;
-   "zoom.us")
+   "zoom.us" | "Zoom")
         icon_result=":zoom:"
         ;;
    "Zotero")
